5eceff6c4fd55e27791ee96bbab815b6c9125a6e
Vlad <QQQqqq123>
2018-03-23 20:23:48 +0500

    move to electron

---
+++ app/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        <h1>Hello World!</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    entry: './app/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        scss: 'vue-style-loader!css-loader!sass-loader'
+                    }
+                }
+            }
+        ]
+    }
+};
